03e17195a9cde57ca0ac722587ade41b9fc7a05f
uailucasSilva <107223850+uailucasSilva@users.noreply.github.com>
2022-10-31 18:56:27 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -20,7 +20,7 @@ function validar() {
 }
 
 function readJSON(usuario, senha) {
-    var file = "json/users.json";
+    file = "json/users.json";
     // var file = "https://wilton-filho.github.io/JS-GitHub/aulas/jogo/login/json/users2.json";
     fetch(file)
         .then(response => response.json())
@@ -37,8 +37,8 @@ function checkUser(content, usuario, senha) {
         }
     }
     if (achou)
-        alertWifi("Usuário válido!", false, 0, 'img/toupeira.gif', 30);
+        alertWifi(`Usuário válido!`, false, 0, "img/toupeira.gif", 30);
     else
-        alertWifi("Usuário inválido!", false, 0, 'img/toupeira.gif', 30);
+        alertWifi(`Usuário inválido!`, false, 0, "img/toupeira.gif", 30);
 
 }
